088b7ecd3a994e1d07c51292f5f0de29c7fb2048
Cedric Quenette <c@cedricq.me>
2021-02-14 14:20:46 +1100

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -18,14 +18,27 @@ const persons = [
     number: "394853894"
   },
   {
-    id: 3,
+    id: 4,
     name: "Mary Poppendick",
     number: "983425"
   }
 ]
 
+const showDateNowInUCT = () => {
+  const nowInUTC = new Date(Date.now()).toUTCString()
+  return `<p>${nowInUTC}</p>`
+}
+
+const showNumberOfPersons = () => {
+  return `<p>Phonebook has info for ${persons.length} people</p>`
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`${showNumberOfPersons()} ${showDateNowInUCT()}`)
 })
 
 const PORT = 3001
