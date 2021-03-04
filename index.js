108b217b4d3e3d833fdbeeb57793ffa1e74d1155
Cedric Quenette <c@cedricq.me>
2021-03-04 19:15:51 +1100

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
+require('dotenv').config()
 const Person = require('./models/person')
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(morgan('tiny'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "9342875"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "934857-8345"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "394853894"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "983425"
-  }
-]
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 const showDateNowInUCT = () => {
   const nowInUTC = new Date(Date.now()).toUTCString()
@@ -54,7 +31,7 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save().then(savedPerson => {
-    response.json(savedPerson)
+    response.json(savedPerson.toJSON())
   })
 })
 
@@ -75,47 +52,29 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
-const generateId = () => {
-  return persons.length > 0 ? Math.floor(Math.random() * 1000000000) : 0
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const findIsNameDuplicate = (personName) => {
-  return !!persons.find(person => person.name === personName)
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
 }
 
-// app.post('/api/persons', (request, response) => {
-//   const body = request.body
-
-//   if(!body.name) {
-//     return response.status(204).json({
-//       error: 'name missing'
-//     })
-//   } else if(!body.number) {
-//     return response.status(204).json({
-//       error: 'number missing'
-//     })
-//   } else if(findIsNameDuplicate(body.name)) {
-//     return response.status(400).json({
-//       error: 'name exists'
-//     })
-//   }
-
-//   const person = {
-//     id: generateId(),
-//     name: body.name,
-//     number: body.number,
-//   }
-
-//   persons.concat(person)
-
-//   response.json(person)
-// })
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
-
+mongoose.set('useFindAndModify', false)
 const url = process.env.MONGODB_URI
 
 console.log(`connecting to ${url}`)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result => {
   console.log('Connected to MongoDB')
-}).catch(error => {
+}).catch((error) => {
   console.log('Error connecting to MongoDB ', error.message)
 })
 
